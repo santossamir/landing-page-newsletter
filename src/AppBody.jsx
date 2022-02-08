2159bd40b5d755766400616b219fdc9484ee3c31
Samir Santos <santossamir@hotmail.com>
2022-02-07 21:50:35 -0300

    Desenvolvendo a sessao compartilhe novidade e a footer.

--- src/AppBody.jsx
+++ src/AppBody.jsx
@@ -71,9 +71,9 @@ export default class Body extends React.Component{
                         </div>
                     </div>
                 </section>
-                <section className="special selection">
-                    <div>
-                        <p>Sua seleção especial</p>
+                <section className="special-selection">
+                    <div className="paragrafo-selec-especial">
+                        <p><span className="p-span">Sua seleção especial</span></p>
                     </div>
                     <div className="container-produtos">
                         
@@ -169,11 +169,139 @@ export default class Body extends React.Component{
                             <button>Comprar</button>
                         </div>
                     </div>
+                    <div className="container-produtos">
+                        
+                        <div className="div-produtos">
+                            <div className="div-img">
+                                <img/>
+                            </div>
+                            <div className="div-nomeDoProduto">
+                                <p>Nome do produto</p>
+                            </div>
+                            <div className="div-descricaoDoProduto">
+                                <p>
+                                    Descrição do produto um pouco maior, com duas linhas
+                                    ou três que explica do que se trata.
+                                </p>
+                                <p style={{margin:"0"}}>De: R$ 23,99</p>
+                            </div>
+                            <div className="div-valorDoProduto">
+                                <p>Por: R$ 19,99</p>
+                            </div>
+                            <div className="div-valorDivido">
+                                <p>ou 2X de R$ 9,99</p>
+                            </div>
+                            <button>Comprar</button>
+                        </div>
+
+                        <div className="div-produtos">
+                            <div className="div-img">
+                                <img/>
+                            </div>
+                            <div className="div-nomeDoProduto">
+                                <p>Nome do produto</p>
+                            </div>
+                            <div className="div-descricaoDoProduto">
+                                <p>
+                                    Descrição do produto um pouco maior, com duas linhas
+                                    ou três que explica do que se trata.
+                                </p>
+                                <p style={{margin:"0"}}>De: R$ 23,99</p>
+                            </div>
+                            <div className="div-valorDoProduto">
+                                <p>Por: R$ 19,99</p>
+                            </div>
+                            <div className="div-valorDivido">
+                                <p>ou 2X de R$ 9,99</p>
+                            </div>
+                            <button>Comprar</button>
+                        </div>
+
+                        <div className="div-produtos">
+                            <div className="div-img">
+                                <img/>
+                            </div>
+                            <div className="div-nomeDoProduto">
+                                <p>Nome do produto</p>
+                            </div>
+                            <div className="div-descricaoDoProduto">
+                                <p>
+                                    Descrição do produto um pouco maior, com duas linhas
+                                    ou três que explica do que se trata.
+                                </p>
+                                <p style={{margin:"0"}}>De: R$ 23,99</p>
+                            </div>
+                            <div className="div-valorDoProduto">
+                                <p>Por: R$ 19,99</p>
+                            </div>
+                            <div className="div-valorDivido">
+                                <p>ou 2X de R$ 9,99</p>
+                            </div>
+                            <button>Comprar</button>
+                        </div>
+
+                        <div className="div-produtos">
+                            <div className="div-img">
+                                <img/>
+                            </div>
+                            <div className="div-nomeDoProduto">
+                                <p>Nome do produto</p>
+                            </div>
+                            <div className="div-descricaoDoProduto">
+                                <p>
+                                    Descrição do produto um pouco maior, com duas linhas
+                                    ou três que explica do que se trata.
+                                </p>
+                                <p style={{margin:"0"}}>De: R$ 23,99</p>
+                            </div>
+                            <div className="div-valorDoProduto">
+                                <p>Por: R$ 19,99</p>
+                            </div>
+                            <div className="div-valorDivido">
+                                <p>ou 2X de R$ 9,99</p>
+                            </div>
+                            <button>Comprar</button>
+                        </div>
+                    </div>
+                    
                     <div className="div-maisProdutos">
                         <button>Ainda mais produtos aqui!</button>
                     </div>
                 </section>
-                
+                <section className="compartilhe-novidade">
+                    <div className="title-comp-novidades">
+                        <p><span className="p-span">Compartilhe a novidade</span></p>
+                    </div>
+                    <div className="paragraph-comp-novidades">
+                        <p>Quer que seus amigos também ganhem a lista personalizada deles?
+                            Preencha agora!
+                        </p>
+                    </div>
+                    <div className="box-form-comp-novidades">
+                        <div className="box-form-nome">
+                            <div className="comp-novidades-form-nome">
+                                <label>Nome do seu amigo:</label>
+                            </div>
+                            <input type="text"/>
+                        </div>
+                        <div className="box-form-email">
+                            <div className="comp-novidades-form-email">
+                                <label>E-mail:</label>
+                            </div>
+                            <input type="email"/>
+                        </div>
+                    </div>
+                    <div className="div-enviarAgora">
+                        <button>Enviar agora</button>
+                    </div>
+                </section>
+                <div className="div-footer">
+                    <div className="div-footer-texts">
+                        <p>Testantando suas habilidades em HTML, CSS e JS</p>
+                        <p>Linx Impulse</p>
+                        <p>2022</p>
+                    </div>
+                </div>
             </div>
         
         )
