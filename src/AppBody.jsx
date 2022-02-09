96499483b9b265e4b967ac500e2c2590209d72c8
Samir Santos <santossamir@hotmail.com>
2022-02-09 13:36:05 -0300

    Adicionando funcionalidade no button enviar agora do form Compartilhe a novidade.

--- src/AppBody.jsx
+++ src/AppBody.jsx
@@ -6,7 +6,8 @@ export default class Body extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            produtos: []
+            produtos: [],
+            produtosTwo: false
         }
     }
     
@@ -17,12 +18,40 @@ export default class Body extends React.Component{
             
         })
       }
+
+    maisProdutos = () =>{   
+        axios.get("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2")
+        .then(resposta =>{
+            this.setState({produtosTwo: resposta.data})
+        })
+
+        this.setState(showState => ({
+            produtosTwo: showState.produtosTwo 
+        }));
+    }
+
+    enviarAgora = () =>{
+        let nomeDoAmigo = document.querySelector("#inputNomeAmigo").value;
+        let emailDoAmigo = document.querySelector("#inputEmailAmigo").value;
+
+        if(nomeDoAmigo === ""){
+            document.querySelector("#inputNomeAmigo").focus();
+        }else if(emailDoAmigo === ""){
+            document.querySelector("#inputEmailAmigo").focus();
+        } else if( emailDoAmigo.indexOf('@') == -1 || emailDoAmigo.indexOf('.com') == -1){
+            alert( "Por favor, informe um E-MAIL válido!" );
+            return false;
+        }else{
+            window.location.reload();
+        }
+    }
     
     render(){
         return(
             <div className="container-body">
                 <section className="algorithm-help">
                     <div className="divText-algorithm-help">
+                        {/*Início da sessão Ajude o algorítimo a ser mais certeiro*/}
                         <div>
                             <div className="divTitle-algorithm-help">
                                 <p>Ajude o algorítimo a ser mais certeiro</p>
@@ -51,7 +80,8 @@ export default class Body extends React.Component{
                                     like Aldus PageMaker including versions of Lorem Ipsum.
                                 </p>
                             </div>
-                        </div> 
+                        </div> {/*Final da sessão Ajude o algorítimo a ser mais certeiro*/}
+                        {/*Início do formulário na sessão Ajude o algorítimo a ser mais certeiro*/}
                         <div className="divForm-algorithm-help">
                             <div className="inputs-texts">
                                 <div className="input-nome">
@@ -84,17 +114,15 @@ export default class Body extends React.Component{
                                 </div>
                             </div>
                             <button>Enviar</button>
-                        </div>
+                        </div> {/*Final do formulário na sessão Ajude o algorítimo a ser mais certeiro*/}
                     </div>
-                </section> 
-
-                {/*Sessão 'seleção especial' de produtos*/} 
-                
+                </section>          
                 {/*Início da sessão 'seleção especial' de produtos*/}
                 <section className="special-selection">
                     <div className="paragrafo-selec-especial">
                         <p><span className="p-span">Sua seleção especial</span></p>
                     </div>
+                    {/*Início da apresentação dos produtos - Parte I*/}
                     <div className="container-produtos">
                         {this.state.produtos.products?.map(item =>(
                             <div className="div-produtos" key={item.id}>
@@ -120,13 +148,38 @@ export default class Body extends React.Component{
                             </div>
                             ))
                         }
-                    </div>  {/*Final da sessão 'seleção especial' de produtos*/}  
-                    
+                    </div>{/*Final da apresentação dos produtos - Parte I*/}
+                    {/*Início da apresentação dos produtos - Parte II*/}
+                    <div className="container-produtos">
+                        {this.state.produtosTwo.products?.map(item =>(
+                            <div className="div-produtos" key={item.id}>
+                                <div className="div-img">
+                                    <img src={item.image}/>
+                                </div>
+                                <div className="div-nomeDoProduto">
+                                    <p>{item.name}</p>
+                                </div>
+                                <div className="div-descricaoDoProduto">
+                                    <p>
+                                        {item.description}
+                                    </p>
+                                    <p style={{margin:"0"}}>De: R$ {item.oldPrice},00</p>
+                                </div>
+                                <div className="div-valorDoProduto">
+                                    <p>Por: R$ {item.price},00</p>
+                                </div>
+                                <div className="div-valorDivido">
+                                    <p>ou {item.installments.count}X de R$ {item.installments.value}0</p>
+                                </div>
+                                <button>Comprar</button>
+                            </div>
+                            ))
+                        }
+                    </div>{/*Final da apresentação dos produtos - Parte II*/}
                     <div className="div-maisProdutos">
-                        <button>Ainda mais produtos aqui!</button>
+                        <button onClick={this.maisProdutos}>{this.state.produtosTwo ? "Ver menos produtos." : "Mais produtos aqui!"}</button>
                     </div>
-                </section> {/* Final - Sessão 'seleção especial' de produtos*/} 
-
+                </section>{/* Final - Sessão 'seleção especial' de produtos*/} 
                 {/*Início da sessão compartilhe a novidade*/} 
                 <section className="compartilhe-novidade">
                     <div className="title-comp-novidades">
@@ -138,24 +191,26 @@ export default class Body extends React.Component{
                             Preencha agora!
                         </p>
                     </div>
-                    <div className="box-form-comp-novidades">
-                        <div className="box-form-nome">
-                            <div className="comp-novidades-form-nome">
-                                <label>Nome do seu amigo:</label>
+                    {/*Início sessão formulário*/}
+                        <div className="box-form-comp-novidades">
+                            <div className="box-form-nome">
+                                <div className="comp-novidades-form-nome">
+                                    <label>Nome do seu amigo:</label>
+                                </div>
+                                <input type="text" id="inputNomeAmigo"/>
                             </div>
-                            <input type="text"/>
-                        </div>
-                        <div className="box-form-email">
-                            <div className="comp-novidades-form-email">
-                                <label>E-mail:</label>
+                            <div className="box-form-email">
+                                <div className="comp-novidades-form-email">
+                                    <label>E-mail:</label>
+                                </div>
+                                <input type="text" name="email" id="inputEmailAmigo"/>
                             </div>
-                            <input type="email"/>
                         </div>
-                    </div>
-                    <div className="div-enviarAgora">
-                        <button>Enviar agora</button>
-                    </div>
-                </section>  {/*Final da sessão compartilhe a novidade*/} 
+                        <div className="div-enviarAgora">
+                            <button onClick={this.enviarAgora}>Enviar agora</button>
+                        </div>
+                    {/*Final da sessão formulário*/}
+                </section>{/*Final da sessão compartilhe a novidade*/} 
             </div>
         )
     }
